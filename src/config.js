c26233f8dd72d90a90064f9cedae0ae08ee74e0e
Martijn van Iersel <mvaniersel@gmail.com>
2020-04-18 22:26:17 +0200

    Fiddle with canvas size

--- src/config.js
+++ src/config.js
@@ -3,11 +3,14 @@ import Phaser from 'phaser';
 export default {
 	type: Phaser.AUTO,
 	parent: 'content',
-	width: 800,
-	height: 600,
+	// width: 800,
+	// height: 600,
 	localStorageName: 'exokeeper',
 	disableContextMenu: true,
 	fps: {
 		target: 20
+	},
+	scale : {
+		mode: Phaser.Scale.RESIZE
 	}
 };
